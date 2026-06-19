057f4e3b2a5971ae15031c72d5fd24cf15173c07
Alam Yotzan Aguilar Chavez <alam-yotzan.aguilar-chavez@etu.univ-amu.fr>
2026-06-19 00:26:30 +0000

    feat(Yahtzee): exercice 5 terminé

--- src/main/java/fr/univ_amu/iut/exercice5/Yahtzee.java
+++ src/main/java/fr/univ_amu/iut/exercice5/Yahtzee.java
@@ -2,13 +2,15 @@ package fr.univ_amu.iut.exercice5;
 
 /// Kata 5 - Yahtzee scoring.
 ///
-/// Chaque méthode statique calcule le score selon sa règle pour un lancer de 5 dés. Le kata est
-/// un classique du refactoring (Yatzy Refactoring Kata) : les solutions "brute-force" fonctionnent
-/// vite, mais vous verrez que certaines règles partagent une structure (compter les occurrences de
-/// chaque face) qu'on peut factoriser.
+/// Chaque méthode statique calcule le score selon sa règle pour un lancer de 5
+/// dés. Le kata est un classique du refactoring (Yatzy Refactoring Kata) : les
+/// solutions "brute-force" fonctionnent vite, mais vous verrez que certaines
+/// règles partagent une structure (compter les occurrences de chaque face) qu'on
+/// peut factoriser.
 ///
-/// Conseil : implémentez chaque méthode en TDD puis, quand plusieurs sont vertes, cherchez une
-/// factorisation. C'est un bon entraînement au TP4 (Refactoring).
+/// Conseil : implémentez chaque méthode en TDD puis, quand plusieurs sont
+/// vertes, cherchez une factorisation. C'est un bon entraînement au
+/// TP4 (Refactoring).
 public class Yahtzee {
 
   private Yahtzee() {}
@@ -16,63 +18,161 @@ public class Yahtzee {
   /// Somme des 5 dés (quelle que soit leur valeur).
   public static int chance(int d1, int d2, int d3, int d4, int d5) {
     int somme = 0;
-    // TODO kata 5 : sommer les 5 dés.
+    somme = d1 + d2 + d3 + d4 + d5;
     return somme;
   }
 
   /// 50 points si les 5 dés sont identiques, 0 sinon.
   public static int yahtzee(int d1, int d2, int d3, int d4, int d5) {
     int score = 0;
-    // TODO kata 5 : 50 si d1 == d2 == d3 == d4 == d5, 0 sinon.
+    if (d1 == d2 && d2 == d3 && d3 == d4 && d4 == d5) // if des[i]==des[i+1]??
+    score = 50;
+    else score = 0;
     return score;
   }
 
-  /// Somme des dés qui montrent la face demandée (utilisé pour ones, twos, ..., sixes).
+  /// Somme des dés qui montrent la face demandée (utilisé pour ones, twos,
+  /// ..., sixes).
   public static int nombres(int face, int[] des) {
     int total = 0;
-    // TODO kata 5 : additionner les dés qui valent 'face'.
+    for (int i = 0; i < des.length; i++) {
+      if (des[i] == face) {
+        total += des[i];
+      }
+    }
     return total;
   }
 
   /// Valeur de la paire la plus haute (2 * valeur). 0 si aucune paire.
   public static int paire(int d1, int d2, int d3, int d4, int d5) {
     int score = 0;
-    // TODO kata 5 : compter les occurrences de chaque face (1..6) puis
-    // renvoyer 2 * la plus haute face qui apparaît au moins 2 fois.
-    return score;
+
+    int[] faces = compterOccurrences(d1, d2, d3, d4, d5);
+
+    for (int i = 5;
+        i >= 0;
+        i--) { // parcours en ordre, donc le premier paire trouve toujours le plus haut
+      if (faces[i] >= 2) {
+        score = 2 * (i + 1);
+        return score;
+      }
+    }
+    return 0;
   }
 
   /// Somme de deux paires de valeurs différentes. 0 sinon.
   public static int deuxPaires(int d1, int d2, int d3, int d4, int d5) {
     int score = 0;
-    // TODO kata 5 : trouver deux faces distinctes qui apparaissent >= 2 fois.
-    return score;
+
+    int[] faces = compterOccurrences(d1, d2, d3, d4, d5);
+    int paires = 0;
+
+    for (int i = 5; i >= 0; i--) {
+      if (faces[i] >= 2) {
+
+        score += 2 * (i + 1);
+        paires++;
+
+        if (paires == 2) {
+          return score;
+        }
+      }
+    }
+    return 0;
   }
 
   /// Somme de 3 dés identiques, 0 sinon.
   public static int brelan(int d1, int d2, int d3, int d4, int d5) {
     int score = 0;
-    // TODO kata 5 : trouver la face qui apparaît au moins 3 fois, renvoyer 3 * face.
-    return score;
+
+    int[] faces = compterOccurrences(d1, d2, d3, d4, d5);
+
+    for (int i = 5; i >= 0; i--) {
+      if (faces[i] >= 3) {
+        score = 3 * (i + 1);
+        return score;
+      }
+    }
+    return 0;
   }
 
   /// 15 si les 5 dés sont exactement 1-2-3-4-5, 0 sinon.
   public static int petiteSuite(int d1, int d2, int d3, int d4, int d5) {
     int score = 0;
-    // TODO kata 5 : vérifier que chaque face de 1 à 5 apparaît exactement une fois.
+    int[] faces = compterOccurrences(d1, d2, d3, d4, d5);
+
+    for (int i = 0; i < 5; i++) {
+      if (faces[i] != 1) {
+        return 0;
+      } else {
+        score = 15;
+      }
+    }
     return score;
   }
 
   /// 20 si les 5 dés sont exactement 2-3-4-5-6, 0 sinon.
   public static int grandeSuite(int d1, int d2, int d3, int d4, int d5) {
+
+    int[] faces = compterOccurrences(d1, d2, d3, d4, d5);
+
+    for (int i = 5; i >= 1; i--) {
+      if (faces[i] != 1) {
+        return 0;
+      }
+    }
+    return 20;
+  }
+
+  /// Somme des 5 dés si on a un brelan plus une paire d'une autre valeur,
+  /// 0 sinon.
+  public static int full(int d1, int d2, int d3, int d4, int d5) {
     int score = 0;
+
+    int[] faces = compterOccurrences(d1, d2, d3, d4, d5);
+    boolean brelan = false;
+    boolean paire = false;
+
+    for (int i = 5; i >= 1; i--) {
+      if (faces[i] == 3) {
+        brelan = true;
+      } else if (faces[i] == 2) {
+        paire = true;
+      }
+      if (paire && brelan) {
+        score = d1 + d2 + d3 + d4 + d5;
+      }
+    }
     return score;
   }
 
-  /// Somme des 5 dés si on a un brelan plus une paire d'une autre valeur, 0 sinon.
-  public static int full(int d1, int d2, int d3, int d4, int d5) {
-    int score = 0;
-    // TODO kata 5 : un brelan (une face 3 fois) + une paire (autre face 2 fois).
-    return score;
+  public static int[] compterOccurrences(int d1, int d2, int d3, int d4, int d5) {
+
+    int[] faces = new int[6];
+    int[] des = new int[] {d1, d2, d3, d4, d5};
+
+    for (int d : des) {
+      switch (d) {
+        case 1:
+          faces[0]++;
+          break;
+        case 2:
+          faces[1]++;
+          break;
+        case 3:
+          faces[2]++;
+          break;
+        case 4:
+          faces[3]++;
+          break;
+        case 5:
+          faces[4]++;
+          break;
+        case 6:
+          faces[5]++;
+          break;
+      }
+    }
+    return faces;
   }
 }
